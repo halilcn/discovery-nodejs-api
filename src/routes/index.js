5dc20bcf0ebb449cbf19eb19b64cb5db621f715d
Halil CAN <halilc.2001@gmail.com>
2021-08-24 22:22:49 +0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+    res.send('ok');
+    return;
+    res.render('index', {title: 'Express'});
+});
+
+module.exports = router;
