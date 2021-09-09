f2cc3fc5c06ce143490d30e39ccdf5d395b6874f
Halil CAN <halilc.2001@gmail.com>
2021-09-09 17:26:51 +0300

    fixed routes and middlewares

--- src/app.js
+++ src/app.js
@@ -1,61 +1,36 @@
-import './bootstrap';
+// Requirements before app starts
+import './bootstrap'
 
-import createError from 'http-errors';
-import express from 'express';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
-import helmet from "helmet";
+import createError from 'http-errors'
+import express from 'express'
 
-import routerIndex from './routes/index';
-import usersRoute from './routes/v1/users';
-import familiesRoute from './routes/v1/families';
-import rolesRoute from './routes/v1/roles';
-import familiesUsersRoute from './routes/v1/familiesUsers';
+var app = express()
 
+//Import middlewares
+require('../src/middlewares')(app)
 
-var app = express();
-
-//Use middlewares
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(cookieParser());
-
-//Use Helmet
-app.use(helmet());
-
-import 'routes/index.js'
-
-/*//Todo:Helper class ?
-const bindFamilyId = (req, res, next) => {
-    req.familyId = req.params.familyId;
-    next();
-};
-
-app.use('/api/v1/users', usersRoute);
-app.use('/api/v1/families', familiesRoute);
-app.use('/api/v1/roles', rolesRoute);
-app.use('/api/v1/families/:familyId/users', bindFamilyId, familiesUsersRoute);*/
+//Import routes
+require('../src/routes')(app)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
-});
+  next(createError(404))
+})
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message
-    res.locals.error = process.env.APP_ENVIRONMENT === 'development' ? err : {}
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = process.env.APP_ENVIRONMENT === 'development' ? err : {}
 
-    console.log(err)
+  console.log(err)
 
-    res.status(err.status || 500)
-    res.send(
-        process.env.APP_ENVIRONMENT === 'development'
-            ? {stack: err.stack, message: err.message}
-            : {message: err.message}
-    )
-});
+  res.status(err.status || 500)
+  res.send(
+    process.env.APP_ENVIRONMENT === 'development'
+      ? { stack: err.stack, message: err.message }
+      : { message: err.message }
+  )
+})
 
-module.exports = app;
+module.exports = app
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,17 @@
-import express from 'express';
-import usersRoute from "./v1/users";
-import familiesRoute from "./v1/families";
-import rolesRoute from "./v1/roles";
-import familiesUsersRoute from "./v1/familiesUsers";
+import usersRoute from './v1/users'
+import familiesRoute from './v1/families'
+import rolesRoute from './v1/roles'
+import familiesUsersRoute from './v1/familiesUsers'
 
-var app = express();
-
-
-//Todo:Helper class ?
+//Bind familyId param
 const bindFamilyId = (req, res, next) => {
-    req.familyId = req.params.familyId;
-    next();
-};
+  req.familyId = req.params.familyId
+  next()
+}
 
-app.use('/api/v1/users', usersRoute);
-app.use('/api/v1/families', familiesRoute);
-app.use('/api/v1/roles', rolesRoute);
-app.use('/api/v1/families/:familyId/users', bindFamilyId, familiesUsersRoute);
-
-export default app;
+module.exports = (app) => {
+  app.use('/api/v1/users', usersRoute)
+  app.use('/api/v1/families', familiesRoute)
+  app.use('/api/v1/roles', rolesRoute)
+  app.use('/api/v1/families/:familyId/users', bindFamilyId, familiesUsersRoute)
+}
